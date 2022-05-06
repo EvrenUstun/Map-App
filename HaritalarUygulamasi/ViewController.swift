0bd90286073dd49d856c3e88308b704a2c82e953
EvrenUstun <ustunnevren@gmail.com>
2022-05-06 10:27:17 +0300

    Initial Commit

---
+++ HaritalarUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritalarUygulamasi
+//
+//  Created by Evren Ustun on 6.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
